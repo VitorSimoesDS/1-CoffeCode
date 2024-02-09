76d88e805411b1be89a1b1c90717bbf6f920a49f
Vitor Simões <159032949+VitorSimoesDS@users.noreply.github.com>
2024-02-09 20:40:24 -0300

    Add files via upload

---
+++ Código/Sistema3.js
@@ -0,0 +1,6 @@
+var multiplying = 10;
+var multiplier = 1;
+
+for (multiplier; multiplier <= 10; multiplier++) {
+    console.log(multiplying + " multiplicador por " + multiplier + " é igual a: " + (multiplying*multiplier)); 
+}
