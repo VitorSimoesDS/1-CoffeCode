006f0cd539aafd75e3f9f1f0e4e4c6ee8c3f0918
Vitor Simões <159032949+VitorSimoesDS@users.noreply.github.com>
2024-02-09 21:16:20 -0300

    CoffeCode

---
+++ Código/4º Atividade/Sistema4.js
@@ -0,0 +1,11 @@
+var number = 0 //Número deve ser váriado para o realização do teste. 
+
+if (number <= 0) {
+    if (number == 0) {
+        console.log("O número é igual a zero.")
+    } else {
+        console.log("O número é menor que zero.")
+    }
+} else {
+    console.log("O número é maior que zero.")
+}
